da3e0115e28e0063e631a299f709cd631a0ca0c1
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-02-22 13:33:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  function getPrimes(base) {
+        var r = base;
+        var obj = {};
+        for (var i = 2; i <= r; i++) {
+            while (r % i == 0) {
+                if (obj[i]) obj[i]++;
+                else obj[i] = 1;
+                r /= i;
+            }
+        }
+        return obj;
+    }
+
+    var primes = getPrimes(base);
+    var results = [];
+
+    for (var key in primes) {
+        var count = 0;
+        var n = number;
+        while (n >= 1) {
+            var temp = Math.floor(n / key);
+            n = temp;
+            count += temp;
+        }
+        results.push(Math.floor(count / primes[key]));
+    }
+
+    return Math.min.apply(null, results);
+};
